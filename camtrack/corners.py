480b1b015292ce761b55269947536b2389f42876
ArChanDDD <povla13@gmail.com>
2021-10-06 20:35:16 +0300

    DZ_1_upgrade_2

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -48,6 +48,16 @@ class _CornerStorageBuilder:
 
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
+    def make_mask(img, corners, min_d):
+        my_mask = np.uint8(np.zeros_like(img) + 255)
+        for j in corners:
+            # print('j= ', j, 'sh = ', img[i].shape)
+            ss = my_mask[max(j[1] - min_d, 0):min(j[1] + min_d, img.shape[0]),
+                 max(j[0] - min_d, 0):min(j[0] + min_d, img.shape[1])].shape
+            my_mask[max(j[1] - min_d, 0):min(j[1] + min_d, img.shape[0]),
+            max(j[0] - min_d, 0):min(j[0] + min_d, img.shape[1])] = np.zeros(ss)
+        return my_mask
+
     # TODO
     ncorners = 5000
     image_0 = frame_sequence[0]
@@ -57,26 +67,37 @@ def _build_impl(frame_sequence: pims.FramesSequence,
     img[2] = cv2.resize(img[1], (0, 0), fx=0.5, fy=0.5)
     img[3] = cv2.resize(img[2], (0, 0), fx=0.5, fy=0.5)
     img[4] = cv2.resize(img[3], (0, 0), fx=0.5, fy=0.5)
+    # print(img[4].shape, img[3].shape, img[2].shape)
     corners0 = np.array([])
+    corners_last = np.array([])
     for i in range(4, -1, -1):
-        corners = cv2.goodFeaturesToTrack(img[i], maxCorners=ncorners, qualityLevel=0.01, minDistance=3, blockSize=5)
-        num_corners = corners.shape[0]
-        corners = corners.reshape(num_corners, 2)
+        my_mask = make_mask(img[i], corners_last.reshape(-1, 2).astype(int), 8)
+        corners = cv2.goodFeaturesToTrack(img[i], maxCorners=ncorners, mask=my_mask, qualityLevel=0.01, minDistance=8,
+                                          blockSize=5)
+        # print(corners)
+        if len(corners_last) == 0:
+            corners_last = corners.reshape(-1, 2) * 2
+        else:
+            corners_last = np.vstack([corners_last * 2, corners.reshape(-1, 2) * 2])
+        corners = corners.reshape(-1, 2)
         corners = corners * (2 ** (i + 1))
         if i == 4:
             corners0 = corners
         else:
             corners0 = np.vstack([corners0, corners])
-    corners1 = cv2.goodFeaturesToTrack(image_0, maxCorners=ncorners, qualityLevel=0.01, minDistance=3,
+
+    my_mask = make_mask(image_0, corners_last.reshape(-1, 2).astype(int), 8)
+    corners1 = cv2.goodFeaturesToTrack(image_0, maxCorners=ncorners, mask=my_mask, qualityLevel=0.01, minDistance=8,
                                        blockSize=5)
     num_corners = corners1.shape[0]
     corners1 = corners1.reshape(num_corners, 2)
     corners0 = np.vstack([corners0, corners1])
     ncorners = corners0.shape[0]
     sizes = np.zeros(ncorners)
+    nums = np.array(np.arange(ncorners))
     sizes.fill(10)
     corners = FrameCorners(
-        np.array(np.arange(ncorners)),
+        nums,
         np.array(corners0),
         sizes
     )
@@ -87,11 +108,26 @@ def _build_impl(frame_sequence: pims.FramesSequence,
                                                        prevPts=corners0,
                                                        nextPts=None,
                                                        maxLevel=5,
-                                                       winSize=(7, 7))
-        # new_corners = cv2.goodFeaturesToTrack(image_1, ncorners, 0.01)
-        corners1 = corners1.reshape(corners1.shape[0], 2)
+                                                       winSize=(7, 7),
+                                                       criteria=(
+                                                           cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+        corners1 = corners1[np.where(_st == 1)[0]]
+        # print(len(np.where(_st == 0)[0]))
+        nums = nums[np.where(_st == 1)[0]]
+        sizes = sizes[np.where(_st == 1)[0]]
+        corners1 = corners1.reshape(-1, 2)
+        max_num = max(nums)
+        my_mask = make_mask(image_1, corners1.astype(int), 10)
+        new_corners = cv2.goodFeaturesToTrack(image_1, maxCorners=int(corners1.shape[0] * 0.025), corners=corners1, mask=my_mask,
+                                              qualityLevel=0.01,
+                                              minDistance=10, blockSize=5)
+        if new_corners is not None:
+            new_corners = new_corners.reshape(-1, 2)
+            corners1 = np.vstack([corners1, new_corners])
+            nums = np.hstack([nums, np.arange(max_num + 1, max_num + 1 + new_corners.shape[0], 1)])
+            sizes = np.hstack([sizes, np.ones(new_corners.shape[0]) + 10])
         corners = FrameCorners(
-            np.array(np.arange(corners1.shape[0])),
+            nums,
             np.array(corners1),
             sizes
         )
